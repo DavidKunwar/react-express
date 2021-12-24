9254616aae49bd3ee207da2e3bf3e7569c719a74
David Kunwar <dkunwar7111@gmail.com>
2021-12-24 21:36:18 +0530

    added Port

--- server/index.js
+++ server/index.js
@@ -6,7 +6,16 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
-// start express server on port 3000
-app.listen(3000, () => {
-  console.log("server started on port 3000");
+// // start express server on port 3000
+// app.listen(3000, () => {
+//   console.log("server started on port 3000");
+// });
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
+    console.log("Server is up and running on " + port);
 });
